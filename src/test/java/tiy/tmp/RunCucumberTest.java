4e5e7eb4a4289f9821b8b94e4c0b798415639580
Igor Timofeev <tiy2000@mail.ru>
2025-10-02 12:45:16 +0700

    UPD: Switch to JUnit 4. WARNING: It doesn't work with 'mvn clean test'!

--- src/test/java/tiy/tmp/RunCucumberTest.java
+++ src/test/java/tiy/tmp/RunCucumberTest.java
@@ -1,15 +1,22 @@
 package tiy.tmp;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber") // Работает без
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "tiy.tmp")   // Работает без
-@SelectClasspathResource("features")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:features",
+        glue = "tiy.tmp"
+)
+//@CucumberOptions(
+//        features = "classpath:features",
+//        glue = "tiy.tmp",
+//        plugin = {
+//                "pretty",
+//                "html:target/cucumber-reports/cucumber-pretty.html",
+//                "json:target/cucumber-reports/CucumberTestReport.json"
+//        }
+//)
 public class RunCucumberTest {
 }
