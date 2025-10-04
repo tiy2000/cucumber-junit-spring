c3fba69de0affda184a358676dd440c96e9aaf48
Igor Timofeev <tiy2000@mail.ru>
2025-10-04 16:30:46 +0700

    UPD: Using tags

--- src/test/java/tiy/tmp/RunCucumberTest.java
+++ src/test/java/tiy/tmp/RunCucumberTest.java
@@ -5,11 +5,14 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber") // Работает без
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "tiy.tmp")   // Работает без
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@Calc")
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@Plus")
 @SelectClasspathResource("features")
 public class RunCucumberTest {
 }
