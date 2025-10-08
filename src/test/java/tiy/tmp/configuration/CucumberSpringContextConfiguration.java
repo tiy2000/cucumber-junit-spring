43916a39cb98aeee441b6cfa843223c4392895b5
Igor Timofeev <tiy2000@mail.ru>
2025-10-08 10:31:12 +0700

    ADD: Spring configuration with @ConditionalOnProperty

--- src/test/java/tiy/tmp/configuration/CucumberSpringContextConfiguration.java
+++ src/test/java/tiy/tmp/configuration/CucumberSpringContextConfiguration.java
@@ -6,16 +6,15 @@ import io.cucumber.java.Scenario;
 import io.cucumber.spring.CucumberContextConfiguration;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import tiy.tmp.TmpCucumber1Application;
 
-import java.util.Arrays;
-import java.util.List;
+// Run with: mvn clean test -Dcfg.platform=android -Dcfg.environment=qt
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // Работает без, но не поднимается WEB
 @ContextConfiguration(classes = TmpCucumber1Application.class)              // Работает без, но не создается бин сервиса
@@ -27,6 +26,84 @@ public class CucumberSpringContextConfiguration {
 
     @PostConstruct
     public void checkProfiles() {
+        new CfgChecker(environment).checkCfgProps();
+    }
+
+
+    @Before
+    public void setup(Scenario scenario) {
+        System.out.println("\nBEFORE: " + scenario.getName());
+        System.out.println("environment=" + environment.getProperty("environment"));
+        System.out.println("platform=" + environment.getProperty("platform"));
+        // This method will be executed before each scenario.
+        // It ensures the Spring context is loaded before tests run.
+    }
+
+    @After
+    public void done(Scenario scenario) {
+        System.out.println("AFTER: " + scenario.getName() + ", status: " + scenario.getStatus());
+        System.out.println();
+    }
+}
+
+
+@Configuration
+@ConditionalOnProperty(name = "cfg.platform", havingValue = "ios")
+//@Profile("ios")
+@PropertySource("classpath:platform-ios.properties")
+class PlatformIOSConfiguration {
+    public PlatformIOSConfiguration() {
+        System.out.println("PlatformIOSConfiguration constructor");
+    }
+}
+
+@Configuration
+@ConditionalOnProperty(name = "cfg.platform", havingValue = "android")
+//@Profile("android")
+@PropertySource("classpath:platform-android.properties")
+class PlatformAndroidConfiguration {
+    public PlatformAndroidConfiguration() {
+        System.out.println("PlatformAndroidConfiguration constructor");
+    }
+}
+
+@Configuration
+//@Profile("local")
+@ConditionalOnProperty(name = "cfg.environment", havingValue = "local")
+@PropertySource("classpath:environment-local.properties")
+class EnvironmentLocalConfiguration {
+    public EnvironmentLocalConfiguration() {
+        System.out.println("EnvironmentLocalConfiguration constructor");
+    }
+}
+
+@Configuration
+//@Profile("et")
+@ConditionalOnProperty(name = "cfg.environment", havingValue = "et")
+@PropertySource("classpath:environment-et.properties")
+class EnvironmentEtConfiguration {
+    public EnvironmentEtConfiguration() {
+        System.out.println("EnvironmentEtConfiguration constructor");
+    }
+}
+
+@Configuration
+//@Profile("qt")
+@ConditionalOnProperty(name = "cfg.environment", havingValue = "qt")
+@PropertySource("classpath:environment-qt.properties")
+class EnvironmentQtConfiguration {
+    public EnvironmentQtConfiguration() {
+        System.out.println("EnvironmentQtConfiguration constructor");
+    }
+}
+
+
+@RequiredArgsConstructor
+class CfgChecker {
+
+    private final Environment environment;
+
+    public void checkCfgProps() {
         StringBuilder errorMessages = new StringBuilder();
         if (!isEnvironmentSpecified()) {
             errorMessages.append("Environment");
@@ -44,74 +121,28 @@ public class CucumberSpringContextConfiguration {
     }
 
     private boolean isEnvironmentSpecified() {
-        List<String> profiles = Arrays.asList(environment.getActiveProfiles());
-        return profiles.contains("local") || profiles.contains("et") || profiles.contains("qt");
+        String environmentProperty = environment.getProperty("cfg.environment");
+        if (environmentProperty == null) {
+            return false;
+        }
+        return switch (environmentProperty) {
+            case "local", "et", "qt" -> true;
+            default -> false;
+        };
     }
 
     private boolean isPlatformSpecified() {
-        List<String> profiles = Arrays.asList(environment.getActiveProfiles());
-        return profiles.contains("android") || profiles.contains("ios");
-    }
-
-    @Before
-    public void setup(Scenario scenario) {
-        System.out.println("\nBEFORE: " + scenario.getName());
-
-        System.out.println("environment=" + environment.getProperty("environment"));
-        System.out.println("platform=" + environment.getProperty("platform"));
-
-        // This method will be executed before each scenario.
-        // It ensures the Spring context is loaded before tests run.
-    }
-
-    @After
-    public void done(Scenario scenario) {
-        System.out.println("AFTER: " + scenario.getName() + ", status: " + scenario.getStatus());
-        System.out.println();
-    }
-}
-
-@Configuration
-@Profile("ios")
-@PropertySource("classpath:platform-ios.properties")
-class PlatformIOSConfiguration {
-    public PlatformIOSConfiguration() {
-        System.out.println("PlatformIOSConfiguration constructor");
-    }
-}
-
-@Configuration
-@Profile("android")
-@PropertySource("classpath:platform-android.properties")
-class PlatformAndroidConfiguration {
-    public PlatformAndroidConfiguration() {
-        System.out.println("PlatformAndroidConfiguration constructor");
-    }
-}
-
-@Configuration
-@Profile("local")
-@PropertySource("classpath:environment-local.properties")
-class EnvironmentLocalConfiguration {
-    public EnvironmentLocalConfiguration() {
-        System.out.println("EnvironmentLocalConfiguration constructor");
-    }
-}
-
-@Configuration
-@Profile("et")
-@PropertySource("classpath:environment-et.properties")
-class EnvironmentEtConfiguration {
-    public EnvironmentEtConfiguration() {
-        System.out.println("EnvironmentEtConfiguration constructor");
-    }
-}
-
-@Configuration
-@Profile("qt")
-@PropertySource("classpath:environment-qt.properties")
-class EnvironmentQtConfiguration {
-    public EnvironmentQtConfiguration() {
-        System.out.println("EnvironmentQtConfiguration constructor");
+        String platformProperty = environment.getProperty("cfg.platform");
+        if (platformProperty == null) {
+            return false;
+        }
+        switch (platformProperty) {
+            case "android", "ios" -> {
+                return true;
+            }
+            default -> {
+                return false;
+            }
+        }
     }
 }
